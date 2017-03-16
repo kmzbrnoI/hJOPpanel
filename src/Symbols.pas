e7250cd7d1d04007cf8fb6789bf60c23bc09d207
Jan Horacek <jan.horacek@seznam.cz>
2017-03-16 17:35:04 +0100

    Doplnena barva symbolu clMarron.
    ...kvuli spravnemu zobrazovani stavu, kdy je vyple DCC.

--- src/Symbols.pas
+++ src/Symbols.pas
@@ -11,10 +11,10 @@ const
 
   //barvy symbolu
   //zde je definovan pocet barev
-  _Symbol_ColorsCount = 12;
+  _Symbol_ColorsCount = 13;
   //zde jsou definovany jednotlive barvy
   _Symbol_Colors: array [0..(_Symbol_ColorsCount-1)] of TColor =
-    ($FF00FF,$A0A0A0,$0000FF,$00FF00,$FFFFFF,$FFFF00,$FF0000,$00FFFF,$000000,$808000,$008080,clPurple);
+    ($FF00FF,$A0A0A0,$0000FF,$00FF00,$FFFFFF,$FFFF00,$FF0000,$00FFFF,$000000,$808000,$008080,clPurple,clMaroon);
 
 
 type
