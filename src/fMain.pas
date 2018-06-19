0eb99e2e3a992188d9cbfabfbdd42ae6d324e18e
Jan Horacek <me@apophis.cz>
2018-06-19 14:41:00 +0200

    Oprava formatovani about okynka.

--- src/fMain.pas
+++ src/fMain.pas
@@ -472,7 +472,7 @@ procedure TF_Main.ShowAboutDialog();
 begin
  Application.MessageBox(PChar('hJOPpanel v'+NactiVerzi(Application.ExeName)+#13#10+
   'build '+GetLastBuildDate()+' '+GetLastBuildTime()+#13#10+
-  'Vytvořil Jan Horáček (c) 2014-2016 pro KMŽ Brno I'), 'Info', MB_OK OR MB_ICONINFORMATION);
+  'Vytvořil Jan Horáček (c) 2014–2018 pro KMŽ Brno I'), 'Info', MB_OK OR MB_ICONINFORMATION);
 end;//procedure
 
 ////////////////////////////////////////////////////////////////////////////////
