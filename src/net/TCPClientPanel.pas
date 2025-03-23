93ed0134442f2f89beb946e195a185909e6b1e4b
Jan Horacek <me@apophis.cz>
2025-03-23 12:41:38 +0100

    HELLO: send app name (new protocol feature).

--- src/net/TCPClientPanel.pas
+++ src/net/TCPClientPanel.pas
@@ -26,6 +26,7 @@ type
   TPanelTCPClient = class
   private const
     _PROTOCOL_VERSION_CLIENT = '1.1';
+    _PROTOCOL_APP_NAME = 'hJOPpanel';
 
   private
     rthread: TReadingThread;
@@ -269,7 +270,7 @@ begin
   Self.pingTimer.Enabled := true;
 
   // send handshake
-  Self.SendLn('-;HELLO;' + Self._PROTOCOL_VERSION_CLIENT + ';');
+  Self.SendLn('-;HELLO;' + Self._PROTOCOL_VERSION_CLIENT + ';' + Self._PROTOCOL_APP_NAME);
 end;
 
 procedure TPanelTCPClient.OnTcpClientDisconnected(Sender: TObject);
